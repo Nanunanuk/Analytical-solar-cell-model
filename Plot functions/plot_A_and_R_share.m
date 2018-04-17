62fec0c24256ab1f898048e1fc5ffe009ef5ca43
Nanunanuk <nils-reiners@gmx.net>
2018-04-17 22:54:16 +0200

    Comments in main software

--- Plot functions/plot_A_and_R_share.m
+++ Plot functions/plot_A_and_R_share.m
@@ -1,5 +1,10 @@
-fig1=figure
-area([A_si_1, R_ext_1, R_esc_1, A_grid_1, A_back_tot_1, A_AR_1, A_FCA_1])
+%% This function is plotting the the share of the different reflection and absorption processes
+% Only the first of the simulated batches will be displayed
 
-fig2=figure
-area([A_si_2, R_ext_2, R_esc_2, A_grid_2, A_back_tot_2, A_AR_2, A_FCA_2])
+fig1=figure
+area(lambda,[A_si_1, R_ext_1, R_esc_1, A_grid_1, A_back_tot_1, A_AR_1, A_FCA_1])
+axis([300 1200 0 1])
+
+L=legend('A_{si}', 'R_{ext}', 'R_{esc}', 'A_{grid}', 'A_{back}', 'A_{AR}', 'A_{FCA}','location','South')
+L.TextColor='w';
+legend boxoff
